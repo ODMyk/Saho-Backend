6e209694f65d02f9c784acc89233fe21470ac9dc
Dmytro Ostapenko <ostapenko.dmytro05@gmail.com>
2024-02-20 15:21:02 +0200

    Changed order of fields in UserEntity

--- Infrastructure.SQL/Database/Entities/UserEntity.cs
+++ Infrastructure.SQL/Database/Entities/UserEntity.cs
@@ -11,19 +11,19 @@ public partial class UserEntity
 
     public int RoleId { get; set; }
 
-    public virtual ICollection<AlbumEntity> Albums { get; set; } = new List<AlbumEntity>();
-
-    public virtual ICollection<PlaylistEntity> PlaylistsNavigation { get; set; } = new List<PlaylistEntity>();
-
     public virtual RoleEntity Role { get; set; } = null!;
-
-    public virtual ICollection<AlbumEntity> AlbumsNavigation { get; set; } = new List<AlbumEntity>();
-
-    public virtual ICollection<UserEntity> Artists { get; set; } = new List<UserEntity>();
-
-    public virtual ICollection<PlaylistEntity> Playlists { get; set; } = new List<PlaylistEntity>();
 
     public virtual ICollection<SongEntity> Songs { get; set; } = new List<SongEntity>();
 
     public virtual ICollection<UserEntity> Users { get; set; } = new List<UserEntity>();
+
+    public virtual ICollection<UserEntity> Artists { get; set; } = new List<UserEntity>();
+
+    public virtual ICollection<AlbumEntity> Albums { get; set; } = new List<AlbumEntity>();
+
+    public virtual ICollection<AlbumEntity> AlbumsNavigation { get; set; } = new List<AlbumEntity>();
+
+    public virtual ICollection<PlaylistEntity> Playlists { get; set; } = new List<PlaylistEntity>();
+
+    public virtual ICollection<PlaylistEntity> PlaylistsNavigation { get; set; } = new List<PlaylistEntity>();
 }
